3cab7b8a78301364cf4d7d74eb75c08c82b9ec12
AUTOGLASS\rielydi.dias <rielydi.dias@autoglass.com.br>
2023-08-08 13:16:16 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Myapp
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var id = Guid.NewGuid();
+            Console.WriteLine(id);
+        }
+
+
+
+
+    }
+
+
+
+}
